d2cf1be80b1e800e72c6cd33f2fabce238614021
night-sornram <sornram.chalernma@gmail.com>
2024-01-28 20:22:08 +0700

    add

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,223 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+    <div className=" font-poppins w-full flex flex-col">
+      <div className=" w-full  flex flex-col bg-custom-200 bg-desktop bg-contain">
+        <div className=" py-10 px-6 md:px-20 w-full px flex justify-between">
+          <div>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            src={"/images/logo.svg"}
+            width={240}
+            height={39}
+            className=" w-40 md:w-40"
+            alt="logo"/>
+          </div>
+          <button className=" py-3 px-6 lg:px-10 xl:px-16 text-xs md:text-xs lg:text-sm xl:text-base shadow-lg  bg-white rounded-full text-custom-300 font-poppins font-semibold">
+            Try It free
+          </button>
+        </div>
+        <div className=" py-20 w-full flex flex-col md:flex-row">
+          <div className=" space-y-7 px-16 md:px-10 lg:px-20 w-full md:w-1/2 flex flex-col justify-center">
+            <div className=" text-center md:text-start text-3xl font-semibold  md:text-3xl lg:text-4xl  xl:text-5xl text-custom-300">
+              Build The Community Your Fans Will Love 
+            </div>
+            <div className="text-center md:text-start text-sm  md:text-xs lg:text-sm  xl:text-base">
+              Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.
+            </div>
+            <button className=" py-3 md:px-6 lg:px-16 md:w-48 lg:w-64 xl:w-80  md:text-xs xl:text-base bg-custom-100 rounded-full text-white font-poppins font-semibold">
+              Get Started For Free
+            </button>
+          </div>
+          <div className=" md:p-0 p-10  md:pr-10 w-full md:w-1/2 flex justify-center items-center">
+            <Image
+            src={"images/illustration-mockups.svg"}
+            width={1035}
+            height={739}
+            alt="mockup"/>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="  border flex flex-col space-y-10 pt-40 pb-80 w-full px-10 md:px-20 ">
+        <div className=" w-full rounded-xl  flex flex-col-reverse md:flex-row shadow-[0_0px_15px_5px_rgba(0,0,0,0.1)]">
+          <div className=" p-5 md:px-10 lg:px-20 space-y-5 w-full md:w-1/2 flex justify-center flex-col">
+            <div className=" text-center md:text-start  md:text-xl lg:text-2xl xl:text-3xl font-semibold text-custom-300">
+              Grow Together
+            </div>
+            <div className="text-center md:text-start   text-xs  lg:text-sm xl:text-base text-custom-400">
+              Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.
+            </div>
+          </div>
+          <div className=" p-5 md:p-10 lg:p-20 w-full md:w-1/2 flex justify-center items-center">
+            <Image
+            src={"/images/illustration-grow-together.svg"}
+            width={1023}
+            height={838}
+            className=" w-[400px]"
+            alt="grow"/>
+          </div>
+        </div>
+        <div className=" w-full rounded-xl  flex flex-col md:flex-row shadow-[0_0px_15px_5px_rgba(0,0,0,0.1)]">
+          
+          <div className=" p-5 md:p-10 lg:p-20 w-full md:w-1/2 flex justify-center items-center">
+            <Image
+            src={"/images/illustration-flowing-conversation.svg"}
+            width={1125}
+            height={801}
+            className=" w-[400px]"
+            alt="flowing"/>
+          </div>
+          <div className=" p-5 md:px-10 lg:px-20 space-y-5 w-full md:w-1/2 flex justify-center flex-col">
+            <div className="text-center md:text-start   md:text-xl lg:text-2xl xl:text-3xl font-semibold text-custom-300">
+              Flowing Conversations
+            </div>
+            <div className="text-center md:text-start    text-xs  md:text-xs  lg:text-sm xl:text-base text-custom-400">
+              You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.
+            </div>
+          </div>
+        </div>
+        <div className=" w-full rounded-xl  flex flex-col-reverse md:flex-row shadow-[0_0px_15px_5px_rgba(0,0,0,0.1)]">
+          <div className=" p-5  md:px-10 lg:px-20 space-y-5 w-full md:w-1/2 flex justify-center flex-col">
+            <div className="text-center md:text-start  md:text-xl lg:text-2xl xl:text-3xl font-semibold text-custom-300">
+              Your Users
+            </div>
+            <div className="text-center md:text-start text-xs md:text-xs  lg:text-sm xl:text-base text-custom-400">
+              It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.
+            </div>
+          </div>
+          <div className=" p-5 md:p-10 lg:p-20 w-full md:w-1/2 flex justify-center items-center">
+            <Image
+            src={"/images/illustration-your-users.svg"}
+            width={1078}
+            height={813}
+            className=" w-[400px]"
+            alt="user"/>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      
+      <div className=" relative px-10 md:px-20 pt-40 space-y-10 md:space-y-5 w-full bg-custom-300 flex flex-col">
+        <div className=" absolute px-10 md:px-0 w-full flex justify-center left-0 -top-20">
+          <div className=" shadow-[0_0px_15px_5px_rgba(0,0,0,0.1)] px-6 md:px-20 py-10 flex justify-center items-center flex-col space-y-5 rounded-md   bg-white ">
+            <div className=" text-custom-300 text-center font-bold text-xl md:text-3xl">
+              Ready To Build Your Community?
+            </div>
+            <button className=" py-3 px-6 lg:px-16  md:w-48 lg:w-64 xl:w-80 text-sm md:text-xs xl:text-base bg-custom-100 rounded-full text-white font-poppins font-semibold">
+              Get Started For Free
+            </button>
+          </div>
+        </div>
+        
+        <div>
+          <Image
+          src={"/images/logo2.svg"}
+          width={240}
+          height={39}
+          alt="logo2"/>
+        </div>
+        <div className="  md:space-x-10 space-y-5 md:space-y-0  w-full flex flex-col md:flex-row">
+          <div className=" w-full md:w-1/4 space-y-5 flex flex-col">
+            <div className=" flex flex-row">
+              <div className=" w-1/12 md:w-1/5">
+                <Image
+                src={"/images/icon-location.svg"}
+                width={18}
+                height={18}
+                className=" bg-fit"
+                alt="location"/>
+              </div>
+              <div className=" w-11/12 md:w-4/5 text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+              </div>
+            </div>
+            <div className=" flex flex-row">
+              <div className="w-1/12 md:w-1/5">
+                <Image
+                src={"/images/icon-phone.svg"}
+                width={18}
+                height={18}
+                className=" "
+                alt="phone"/>
+              </div>
+              <div className=" w-11/12 md:w-4/5 text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base">
+                +1-543-123-4567
+              </div>
+            </div>
+            <div className=" flex flex-row">
+              <div className=" w-1/12 md:w-1/5">
+                <Image
+                src={"/images/icon-email.svg"}
+                width={18}
+                height={18}
+                className=" "
+                alt="email"/>
+              </div>
+              <div className=" w-11/12 md:w-4/5 text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base">
+                example@huddle.com
+              </div>
+            </div>
+          </div>
+          <div className=" pt-5 md:pt-0 w-full md:w-1/4  md:items-center  text-custom-200 space-y-5 flex flex-col">
+            <Link className=" text-sm  text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              About US
+            </Link>
+            <Link className=" text-sm  text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              What We Do
+            </Link>
+            <Link className=" text-sm   text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              FAQ
+            </Link>
+          </div>
+          <div className="  w-full md:w-1/4 md:items-center  text-custom-200 space-y-5 flex flex-col">
+            <Link className=" text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              Career
+            </Link>
+            <Link className=" text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+             Blog
+            </Link>
+            <Link className=" text-sm text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              Contact Us
+            </Link>
+          </div>
+          <div className="  pt-5 md:pt-0 w-full md:w-1/4   text-custom-200  space-x-7 md:space-x-0 justify-center md:justify-around flex flex-row">
+            <Link className=" text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              <div className=" border border-white rounded-full p-2"> 
+                <Image
+                src={"/images/facebook.svg"}
+                width={10}
+                height={10}
+                className=" w-4 h-4"
+                alt="facebook"/>
+              </div>
+            </Link>
+            <Link className=" text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              <div className=" border border-white rounded-full p-2"> 
+                <Image
+                src={"/images/x.svg"}
+                width={10}
+                height={10}
+                className=" w-4 h-4"
+                alt="x"/>
+              </div>
+            </Link>
+            <Link className=" text-custom-200 md:text-xs lg:text-sm xl:text-base" href={"/"}>
+              <div className=" border border-white rounded-full p-2"> 
+                <Image
+                src={"/images/instagram.svg"}
+                width={10}
+                height={10}
+                className=" w-4 h-4"
+                alt="instagram"/>
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className="text-sm md:text-xs lg:text-sm xl:text-base pb-10 text-custom-200 flex items-center justify-center md:justify-end">
+          © Copyright 2018 Huddle. All rights reserved.
+        </div>
       </div>
-    </main>
-  );
-}
+    </div>
+    
+  )}
+   
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,20 @@
 import type { Config } from "tailwindcss";
 
+const {colors : defaultColors } = require("tailwindcss/defaultTheme")
+
+const colors = 
+{
+  ...defaultColors,
+  ...{
+    "custom" : {
+      "100" : "hsl(322, 100%, 66%)",
+      "200" : "hsl(193, 100%, 96%)",
+      "300" : "hsl(192, 100%, 9%)",
+      "400" : "hsl(208, 11%, 55%)"
+    }
+  }
+}
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,7 +27,13 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+          "desktop" : "url('/images/bg-hero-desktop.svg')"
       },
+      colors : colors,
+      fontFamily : {
+        "poppins" : 'Poppins',
+        'opensan' : 'Open Sans'
+      }
     },
   },
   plugins: [],
